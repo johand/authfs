2143e5f84ab04310f772715b43e4df3564acc1e5
Johan <johpunk@gmail.com>
2012-07-30 03:54:04 -0430

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+**tutorial from nettuts+ [source](http://net.tutsplus.com/tutorials/authentication-in-rails-from-scratch/)**
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '7ff98be33c95abd967f41ef044d9883f115a0cc76042829f129c91303af80a9780e688ded4905f9c00ae471afdff68bbbac3a1ab37ce2c21f7db0501a002516b'
